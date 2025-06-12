fd9524bc51f443f4a002cb753f34123102508320
Joseph Smith <smithj28@oregonstate.edu>
2025-06-12 16:05:32 -0500

    fixed an inssue with restart writing?

--- mediator/med_phases_restart_mod.F90
+++ mediator/med_phases_restart_mod.F90
@@ -16,7 +16,6 @@ module med_phases_restart_mod
   use pio                     , only : file_desc_t
 #ifndef CESMCOUPLED
   use shr_is_restart_fh_mod   , only : init_is_restart_fh, is_restart_fh, is_restart_fh_type
-!  use shr_is_restart_fh_mod   , only : log_restart_fh
 #endif
   use shr_log_mod             , only : shr_log_error
 
@@ -253,10 +252,6 @@ contains
        endif
     endif
 
-#ifndef CESMCOUPLED
-    call is_restart_fh(clock, restartfh_info, write_restartfh)
-    if (write_restartfh) alarmIsOn = .true.
-#endif
 
     if (alarmIsOn) then
        call ESMF_ClockGet(clock, currtime=currtime, starttime=starttime, rc=rc)
@@ -492,8 +487,8 @@ contains
        call med_io_close(io_file, rc=rc)
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
 #ifndef CESMCOUPLED
-       !call log_restart_fh(nextTime, startTime, 'cmeps', rc=rc)
-       !if (ChkErr(rc,__LINE__,u_FILE_u)) return
+       call log_restart_fh(nextTime, startTime, 'cmeps', rc=rc)
+       if (ChkErr(rc,__LINE__,u_FILE_u)) return
 #endif
     endif
 
