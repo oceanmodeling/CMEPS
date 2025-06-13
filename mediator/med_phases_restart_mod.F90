ed28fcdd64a0b8f170c606035e02e8d35cd3e6b3
Joseph Smith <smithj28@oregonstate.edu>
2025-06-12 19:02:44 -0500

    reverted mediator/med_phases_restart_mod.F90 to match latest coastal version

--- mediator/med_phases_restart_mod.F90
+++ mediator/med_phases_restart_mod.F90
@@ -16,6 +16,7 @@ module med_phases_restart_mod
   use pio                     , only : file_desc_t
 #ifndef CESMCOUPLED
   use shr_is_restart_fh_mod   , only : init_is_restart_fh, is_restart_fh, is_restart_fh_type
+  use shr_is_restart_fh_mod   , only : log_restart_fh
 #endif
   use shr_log_mod             , only : shr_log_error
 
@@ -252,6 +253,11 @@ contains
        endif
     endif
 
+#ifndef CESMCOUPLED
+    call is_restart_fh(clock, restartfh_info, write_restartfh)
+    if (write_restartfh) alarmIsOn = .true.
+#endif
+
     if (alarmIsOn) then
        call ESMF_ClockGet(clock, currtime=currtime, starttime=starttime, rc=rc)
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
@@ -485,6 +491,10 @@ contains
        ! Close file
        call med_io_close(io_file, rc=rc)
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
+#ifndef CESMCOUPLED
+       call log_restart_fh(nextTime, startTime, 'cmeps', rc=rc)
+       if (ChkErr(rc,__LINE__,u_FILE_u)) return
+#endif
     endif
 
     !---------------------------------------
